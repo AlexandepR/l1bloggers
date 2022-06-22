4df047f0ad03653bf43a52a92ae4a4e76c0bd4f7
Alex <alexandepp@gmail.com>
2022-06-22 13:12:02 +0300

    init

--- src/middleware/input-validation-middleware.ts
+++ src/middleware/input-validation-middleware.ts
@@ -6,22 +6,23 @@ import {youtubeUrlValidator} from "../routes/bloggers-router";
 export const inputValidationMiddleware = (
     req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req)
-    let errorMessages = errors.array().map(err => ({
+    let errorsMessages = errors.array().map(err => ({
         message: err.msg,
         field: err.param
     }))
-    errorMessages = errorMessages.filter((err, index) => {
-        const findIndex = errorMessages.findIndex((innerErr => innerErr.field === err.field))
-        return findIndex === index
-    })
-
     if (!errors.isEmpty()) {
         res.status(400)
         res.send({
-            errorMessages
+            errorsMessages
         })
         return
     } else {
         next()
     }
 }
+
+
+// errorsMessages = errorMessages.filter((err, index) => {
+//     const findIndex = errorMessages.findIndex((innerErr => innerErr.field === err.field))
+//     return findIndex === index
+// })
