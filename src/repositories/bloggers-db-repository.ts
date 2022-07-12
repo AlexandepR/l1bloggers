ac40ea13f932dfd01d8c7acb798aa54e856ac51c
Alex <alexandepp@gmail.com>
2022-07-12 16:06:52 +0300

    blog-db-rep_promise

--- src/repositories/bloggers-db-repository.ts
+++ src/repositories/bloggers-db-repository.ts
@@ -1,3 +1,8 @@
+// import {client} from "mongodb";
+
+import {client} from "./db";
+import {bloggers} from "./bloggers-memory-repository";
+
 export let __bloggers = [
     {id: 0, name: "Petya", youtubeUrl: "https://www.youtube.com/uOWp8HU"},
     {id: 1, name: "Vasya", youtubeUrl: "https://www.youtube.com/uO00tr"},
@@ -11,6 +16,7 @@ type bloggersType = {
 
 export const bloggersRepository = {
     async getBloggers(): Promise<bloggersType[]> {
+
         // async getBloggers(name: string | null | undefined): Promise<bloggersType[]> {
         //     if (name) {
         //         let filteredBloggers = bloggers.filter(b => b.name.indexOf(name) > -1)
@@ -18,10 +24,12 @@ export const bloggersRepository = {
         //     } else {
         //         return bloggers
         //     }
-        return __bloggers
+        return client.db("allBloggers").collection<bloggersType>("bloggers").find({}).toArray()
     },
     async getBloggerByID(id: number): Promise<bloggersType | null> {
-        const blogger = __bloggers.find(b => b.id === id)
+        // const blogger = __bloggers.find(b => b.id === id)
+        let blogger: bloggersType | null =
+            await client.db("allBloggers").collection<bloggersType>("bloggers").findOne({id})
         if(blogger) {
             return blogger
         } else {
@@ -34,26 +42,31 @@ export const bloggersRepository = {
             name: name,
             youtubeUrl: youtubeUrl
         }
-        __bloggers.push(newBlogger)
+        const blogger = await client.db("allBloggers").collection<bloggersType>("bloggers").insertOne(newBlogger)
+        // __bloggers.push(newBlogger)
         return newBlogger
     },
     async putBlogger(id: number, name: string, youtubeUrl: string): Promise<boolean> {
-        const blogger = __bloggers.find(b => b.id === id)
-        if (blogger) {
-            blogger.name = name;
-            blogger.youtubeUrl = youtubeUrl;
-            return true
-        } else {
-            return false
-        }
+        const updateBlogger = await client.db("allBloggers").collection<bloggersType>("bloggers").updateOne({id: id}, { $set: {name: name, youtubeUrl: youtubeUrl}})
+        return updateBlogger.matchedCount === 1
+        // const blogger = __bloggers.find(b => b.id === id)
+        // if (blogger) {
+        //     blogger.name = name;
+        //     blogger.youtubeUrl = youtubeUrl;
+        //     return true
+        // } else {
+        //     return false
+        // }
     },
     async deleteBlogger(id: number): Promise<boolean> {
-        for (let i = 0; i < __bloggers.length; i++) {
-            if (__bloggers[i].id === id) {
-                __bloggers.splice(i, 1)
-                return true
-            }
-        }
-        return false
+        const delBlogger = await client.db("allBloggers").collection<bloggersType>("bloggers").deleteOne({id: id})
+        return delBlogger.deletedCount === 1
+        // for (let i = 0; i < __bloggers.length; i++) {
+        //     if (__bloggers[i].id === id) {
+        //         __bloggers.splice(i, 1)
+        //         return true
+        //     }
+        // }
+        // return false
     }
 }
