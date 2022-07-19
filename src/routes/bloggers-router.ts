89d34f93aec6251a32b6bc0c7a0c9ae874531cb3
Alex <alexandepp@gmail.com>
2022-07-19 21:20:55 +0300

    add_paginat-blogg

--- src/routes/bloggers-router.ts
+++ src/routes/bloggers-router.ts
@@ -87,7 +87,8 @@ bloggersRouter.post('/', authMiddleware, nameValidation, youtubeUrlValidator, in
         const newBlogger = await bloggersService.createBlogger(req.body.name, req.body.youtubeUrl);
         if (newBlogger) {
             res.status(201)
-            res.send( {id: newBlogger.id, name: newBlogger.name, youtubeUrl: newBlogger.youtubeUrl})
+            // res.send( {id: newBlogger.id, name: newBlogger.name, youtubeUrl: newBlogger.youtubeUrl})
+            // res.send( newBlogger)
         } else (
             res.sendStatus(400)
         )
