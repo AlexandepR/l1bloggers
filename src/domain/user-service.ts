b7762951f6a60d66955cd4e7dbb505b909df635a
Alex <alexandepp@gmail.com>
2022-07-28 23:34:40 +0300

    add user-service

--- src/domain/user-service.ts
+++ src/domain/user-service.ts
@@ -1,9 +1,12 @@
-import {userType} from "../repositories/db";
-import {usersRepository} from "../repositories/users-repository";
+import {UserType} from "../repositories/db";
+import {resultType, usersRepository} from "../repositories/users-repository";
 
 
 export const usersService = {
-    async getUsers(pageNumber: number, pageSize: number): Promise<{ pagesCount: number }> {
+    async getUsers(pageNumber: number, pageSize: number): Promise<resultType<UserType[]>> {
        return usersRepository.getUsers(pageNumber, pageSize)
+    },
+    async createService(login: string, password: string): Promise<any> {
+        return usersRepository.createUser(login, password)
     }
 }
