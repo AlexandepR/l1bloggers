d7ac12eecfc81e6ada2985fb6d451b537c32ae83
Alex <alexandepp@gmail.com>
2022-07-29 12:07:12 +0300

    add_auth-service

--- src/domain/auth-servise.ts
+++ src/domain/auth-servise.ts
@@ -8,6 +8,7 @@ import bcrypt from 'bcrypt'
 export const authService = {
     async generateHash(password:string) {
         const hash = await bcrypt.hash(password, 10)
+        return hash
     }
 }
 
