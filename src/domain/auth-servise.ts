449bb51ea063f4f0d0caf50e5e4e57498d874382
Alex <alexandepp@gmail.com>
2022-07-29 23:02:48 +0300

    auth-service_change

--- src/domain/auth-servise.ts
+++ src/domain/auth-servise.ts
@@ -7,7 +7,10 @@ import bcrypt from 'bcrypt'
 
 export const authService = {
     async generateHash(password:string) {
+        // const passwordSalt = await bcrypt.genSalt(10)
+        // const passwordHash = await this._generateHash(password, passwordSalt)
         const hash = await bcrypt.hash(password, 10)
+
         return hash
     }
 }
