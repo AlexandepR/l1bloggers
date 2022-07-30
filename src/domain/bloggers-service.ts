ed297d4da054d3b91161b8f827e0c7571ff9d4e1
Alex <alexandepp@gmail.com>
2022-07-30 20:05:16 +0300

    blog-service_change

--- src/domain/bloggers-service.ts
+++ src/domain/bloggers-service.ts
@@ -1,22 +1,27 @@
 import {bloggersDataType, bloggersRepository} from "../repositories/bloggers-db-repository";
 import {BloggersType, PostsType} from "../repositories/db";
 import {postsRepository} from "../repositories/posts-db-repository";
+import {ObjectId} from "mongodb";
 
 
 export const bloggersService = {
     async getBloggers(pageNumber: number, pageSize: number, searchNameTerm: string | null): Promise<bloggersDataType<BloggersType[]>> {
-        return bloggersRepository.getBloggers(pageNumber, pageSize, searchNameTerm)
+        const bloggers = bloggersRepository.getBloggers(pageNumber, pageSize, searchNameTerm)
+        return bloggers
     },
     async getBloggerByID(id: number): Promise<BloggersType | null> {
         return bloggersRepository.getBloggerByID(id)
     },
     async createBlogger(name: string, youtubeUrl: string): Promise<BloggersType> {
+        // const newId = new ObjectId()
+        // console.log( typeof (newId))
         const newBlogger = {
-            id: +(new Date()),
-            name: name,
-            youtubeUrl: youtubeUrl
+            // _id: +new ObjectId().toString,
+            // id: +(new Date()),
+            name,
+            youtubeUrl
         }
-        const createdBlogger = await bloggersRepository.postBlogger(newBlogger)
+        const createdBlogger = await bloggersRepository.createBlogger(newBlogger)
         return createdBlogger
     },
     async createPostForBlogger(bloggerId: number, title: string,
